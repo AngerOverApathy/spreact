6ceb67869388ca74309fe69a80e094179541d87c
AngerOverApathy <violasian@gmail.com>
2024-03-25 17:37:56 -0700

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.spaceflightnewsapi.net/v3/articles')
+      .then(response => response.json())
+      .then(data => {
+        setNews(data)
+      })
+  })
 
   return (
     <>
       <div className='App'>
-        <h1>Space News</h1>
+        <div className='title'>
+          <h1>Space News</h1>
+        </div>
+        <div className='newsContainer'>
+          {news.map((val, key) => {
+            return <div key={key}> {val.title} </div>
+          })}
+        </div>
       </div>
     </>
   )
